ba481316783c50d97a21fa39ea0b31ade9b89023
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2026-02-27 22:07:27 +0200

    Objects

--- src/index.js
+++ src/index.js
@@ -1,23 +1,84 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const user = {
+    name: "Nataliia",
+    email: "Natali@gmail.com",
+    password: "123456789",
+};
 const products = [
-    { title: "Product 1", price: 10 },
-    { title: "Product 2", price: 20 },
-    { title: "Product 3", price: 30 },
+    {
+        name: "Product 1",
+        price: 10,
+        quantity: 5,
+    },
+    {
+        name: "Product 2",
+        price: 20,
+        quantity: 3,
+    },
+    {
+        name: "Product 3",
+        price: 30,
+        quantity: 2,
+    },
 ];
-function calculateTotalPrice(...products) {
-    if (products.length === 0) {
-        console.log("List products is empty");
-        return 0;
-    }
-    for (let product of products) {
-        if (product.price < 0) {
-            console.log("Proce has not be 0");
-            return 0;
-        }
-    }
-    const totalPrice = products.reduce((accum, currentValue) => {
-        return accum + currentValue.price;
-    }, 0);
-    return totalPrice;
+const contacts = [
+    { name: "Nataliia", phone: "1234567890" },
+    { name: "Roll", email: "roll@gmail.com" },
+    { name: "Lary" },
+];
+function createContact(name, phone, email) {
+    return {
+        name,
+        phone,
+        email,
+    };
 }
-const total = calculateTotalPrice(...products);
-console.log("Total price:", total);
+const newContact = createContact("Jon", "5555555555", "jon@gmail.com");
+// console.log(newContact)
+const contactGroup = [
+    { name: "friends", contacts: [contacts[0]] },
+    { name: "family", contacts: [contacts[1]] },
+];
+// console.log(contactGroup)
+function addToGroup(contact, nameGroup) {
+    const group = contactGroup.find((group) => {
+        if (group.name === nameGroup) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    });
+    if (group) {
+        group.contacts.push(contact);
+    }
+    else {
+        console.log("Group not found");
+    }
+}
+addToGroup(newContact, "friends");
+addToGroup(contacts[2], "family");
+function fetchUser() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield fetch("https://randomuser.me/api?results=100");
+            const data = yield response.json();
+            return data.results;
+        }
+        catch (error) {
+            console.log(error);
+            return [];
+        }
+    });
+}
+fetchUser().then((users) => {
+    console.log(users);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,111 @@
+const user: { name: string; email: string; password: string } = {
+  name: "Nataliia",
+  email: "Natali@gmail.com",
+  password: "123456789",
+};
+
+// console.log(user);
 
 type Product = {
-  title: string;
+  name: string;
   price: number;
+  quantity: number;
 };
 
 const products: Product[] = [
-  { title: "Product 1", price: 10 },
-  { title: "Product 2", price: 20 },
-  { title: "Product 3", price: 30 },
+  {
+    name: "Product 1",
+    price: 10,
+    quantity: 5,
+  },
+  {
+    name: "Product 2",
+    price: 20,
+    quantity: 3,
+  },
+  {
+    name: "Product 3",
+    price: 30,
+    quantity: 2,
+  },
 ];
 
-function calculateTotalPrice(...products: Product[]): number {
-  if (products.length === 0) {
-    console.log("List products is empty");
-    return 0;
-  }
-  for (let product of products) {
-    if (product.price < 0) {
-      console.log("Proce has not be 0");
-      return 0;
-    }
-  }
-  const totalPrice = products.reduce((accum, currentValue) => {
-    return accum + currentValue.price;
-  }, 0);
-  return totalPrice;
+// console.log(products)
+
+type Contact = {
+  name: string;
+  phone?: string;
+  email?: string;
+};
+
+type ContactGroup = {
+  name: string;
+  contacts: Contact[];
+};
+
+const contacts: Contact[] = [
+  { name: "Nataliia", phone: "1234567890" },
+  { name: "Roll", email: "roll@gmail.com" },
+  { name: "Lary" },
+];
+
+function createContact(name: string, phone?: string, email?: string): Contact {
+  return {
+    name,
+    phone,
+    email,
+  };
 }
 
-const total = calculateTotalPrice(...products);
-console.log("Total price:", total);
+const newContact = createContact("Jon", "5555555555", "jon@gmail.com");
+// console.log(newContact)
+
+const contactGroup: ContactGroup[] = [
+  { name: "friends", contacts: [contacts[0]] },
+  { name: "family", contacts: [contacts[1]] },
+];
+
+// console.log(contactGroup)
+
+function addToGroup(contact: Contact, nameGroup: string) {
+  const group = contactGroup.find((group) => {
+    if (group.name === nameGroup) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  if (group) {
+    group.contacts.push(contact);
+  } else {
+    console.log("Group not found");
+  }
+}
+
+addToGroup(newContact, "friends");
+addToGroup(contacts[2], "family");
+
+// console.log(contactGroup);
+
+type User = {
+  name: {
+    firstName: string;
+    lastName: string;
+  };
+  email: string;
+};
+
+async function fetchUser(): Promise<User[]> {
+  try {
+    const response = await fetch("https://randomuser.me/api?results=100");
+    const data: { results: User[] } = await response.json();
+    return data.results;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
+
+fetchUser().then((users: User[]) => {
+  console.log(users);
+});
