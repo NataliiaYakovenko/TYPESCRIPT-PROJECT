f05e6853a671771a3cb42471ada674d25db66fb9
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2026-03-04 20:22:54 +0200

    Objects Home task

--- src/index.js
+++ src/index.js
@@ -1,84 +1,37 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-const user = {
-    name: "Nataliia",
-    email: "Natali@gmail.com",
-    password: "123456789",
-};
-const products = [
-    {
-        name: "Product 1",
-        price: 10,
-        quantity: 5,
-    },
-    {
-        name: "Product 2",
-        price: 20,
-        quantity: 3,
-    },
-    {
-        name: "Product 3",
-        price: 30,
-        quantity: 2,
-    },
-];
-const contacts = [
-    { name: "Nataliia", phone: "1234567890" },
-    { name: "Roll", email: "roll@gmail.com" },
-    { name: "Lary" },
-];
-function createContact(name, phone, email) {
-    return {
-        name,
-        phone,
-        email,
-    };
-}
-const newContact = createContact("Jon", "5555555555", "jon@gmail.com");
-// console.log(newContact)
-const contactGroup = [
-    { name: "friends", contacts: [contacts[0]] },
-    { name: "family", contacts: [contacts[1]] },
-];
-// console.log(contactGroup)
-function addToGroup(contact, nameGroup) {
-    const group = contactGroup.find((group) => {
-        if (group.name === nameGroup) {
-            return true;
+function calculateExpenses(purchases) {
+    let total = 0;
+    let cashTotal = 0;
+    let cardTotal = 0;
+    for (const purchase of purchases) {
+        total += purchase.price;
+        if (purchase.paymentMethod === "cash") {
+            cashTotal += purchase.price;
         }
         else {
-            return false;
+            cardTotal += purchase.price;
         }
-    });
-    if (group) {
-        group.contacts.push(contact);
     }
-    else {
-        console.log("Group not found");
-    }
+    return {
+        total,
+        cardTotal,
+        cashTotal,
+    };
 }
-addToGroup(newContact, "friends");
-addToGroup(contacts[2], "family");
-function fetchUser() {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const response = yield fetch("https://randomuser.me/api?results=100");
-            const data = yield response.json();
-            return data.results;
-        }
-        catch (error) {
-            console.log(error);
-            return [];
-        }
-    });
-}
-fetchUser().then((users) => {
-    console.log(users);
-});
+const purchases = [
+    {
+        product: "Laptop",
+        price: 1000,
+        paymentMethod: "card",
+    },
+    {
+        product: "Phone",
+        price: 100,
+        paymentMethod: "cash",
+    },
+    {
+        product: "mouse",
+        price: 50,
+        paymentMethod: "cash",
+    },
+];
+console.log(calculateExpenses(purchases));
--- src/index.ts
+++ src/index.ts
@@ -1,111 +1,51 @@
-const user: { name: string; email: string; password: string } = {
-  name: "Nataliia",
-  email: "Natali@gmail.com",
-  password: "123456789",
-};
-
-// console.log(user);
-
-type Product = {
-  name: string;
+type Purchase = {
+  product: string;
   price: number;
-  quantity: number;
+  paymentMethod: "cash" | "card";
 };
 
-const products: Product[] = [
-  {
-    name: "Product 1",
-    price: 10,
-    quantity: 5,
-  },
-  {
-    name: "Product 2",
-    price: 20,
-    quantity: 3,
-  },
-  {
-    name: "Product 3",
-    price: 30,
-    quantity: 2,
-  },
-];
+function calculateExpenses(purchases: Purchase[]): {
+  total: number;
+  cashTotal: number;
+  cardTotal: number;
+} {
+  let total = 0;
+  let cashTotal = 0;
+  let cardTotal = 0;
 
-// console.log(products)
+  for (const purchase of purchases) {
+    total += purchase.price;
 
-type Contact = {
-  name: string;
-  phone?: string;
-  email?: string;
-};
-
-type ContactGroup = {
-  name: string;
-  contacts: Contact[];
-};
-
-const contacts: Contact[] = [
-  { name: "Nataliia", phone: "1234567890" },
-  { name: "Roll", email: "roll@gmail.com" },
-  { name: "Lary" },
-];
-
-function createContact(name: string, phone?: string, email?: string): Contact {
-  return {
-    name,
-    phone,
-    email,
-  };
-}
-
-const newContact = createContact("Jon", "5555555555", "jon@gmail.com");
-// console.log(newContact)
-
-const contactGroup: ContactGroup[] = [
-  { name: "friends", contacts: [contacts[0]] },
-  { name: "family", contacts: [contacts[1]] },
-];
-
-// console.log(contactGroup)
-
-function addToGroup(contact: Contact, nameGroup: string) {
-  const group = contactGroup.find((group) => {
-    if (group.name === nameGroup) {
-      return true;
+    if (purchase.paymentMethod === "cash") {
+      cashTotal += purchase.price;
     } else {
-      return false;
+      cardTotal += purchase.price;
     }
-  });
-  if (group) {
-    group.contacts.push(contact);
-  } else {
-    console.log("Group not found");
   }
-}
 
-addToGroup(newContact, "friends");
-addToGroup(contacts[2], "family");
-
-// console.log(contactGroup);
-
-type User = {
-  name: {
-    firstName: string;
-    lastName: string;
+  return {
+    total,
+    cardTotal,
+    cashTotal,
   };
-  email: string;
-};
-
-async function fetchUser(): Promise<User[]> {
-  try {
-    const response = await fetch("https://randomuser.me/api?results=100");
-    const data: { results: User[] } = await response.json();
-    return data.results;
-  } catch (error) {
-    console.log(error);
-    return [];
-  }
 }
 
-fetchUser().then((users: User[]) => {
-  console.log(users);
-});
+const purchases: Purchase[] = [
+  {
+    product: "Laptop",
+    price: 1000,
+    paymentMethod: "card",
+  },
+  {
+    product: "Phone",
+    price: 100,
+    paymentMethod: "cash",
+  },
+  {
+    product: "mouse",
+    price: 50,
+    paymentMethod: "cash",
+  },
+];
+
+console.log(calculateExpenses(purchases));
