e71774af5b59885d54f41cd95a63df33f5010a51
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2026-03-31 15:51:46 +0300

    Inheritance

--- src/index.js
+++ src/index.js
@@ -1,39 +1,16 @@
-class Car {
-    constructor(manufacturer, maxSpeed) {
-        this._manufacturer = manufacturer;
-        this._speed = 0;
-        this._maxSpeed = maxSpeed;
-    }
-    accelearate(speedDelta) {
-        if (this._speed + speedDelta <= this._maxSpeed) {
-            this._speed += speedDelta;
-            return this._speed;
-        }
-        this._speed = this._maxSpeed;
-        return this._speed;
-    }
-    decelerate(speedDelta) {
-        if (this._speed - speedDelta >= 0) {
-            this._speed -= speedDelta;
-            return this._speed;
-        }
-        this._speed = 0;
-        return this._speed;
-    }
-    get speed() {
-        return this._speed;
-    }
-    get manufacturer() {
-        return this._manufacturer;
-    }
-    set manufacturer(value) {
-        this._manufacturer = value;
-    }
-    get maxSpeed() {
-        return this._maxSpeed;
+class Animal {
+    constructor(name) {
+        this.name = name;
     }
 }
-const car = new Car('Lexus', 250);
-car.accelearate(150);
-car.decelerate(303);
-console.log(car.speed);
+class Dog extends Animal {
+    constructor(name, breed) {
+        super(name);
+        this.breed = breed;
+    }
+    makeSound() {
+        return `${this.name} barks`;
+    }
+}
+const dog = new Dog("Pet", "Labrador");
+console.log(dog.makeSound());
--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,21 @@
-class Car {
-  private _manufacturer: string;
-  private _speed: number;
-  private readonly _maxSpeed: number;
+abstract class Animal {
+  constructor(public name: string) {}
+  abstract makeSound(): string;
+}
 
-  constructor(manufacturer: string, maxSpeed: number) {
-    this._manufacturer = manufacturer;
-    this._speed = 0;
-    this._maxSpeed = maxSpeed;
+class Dog extends Animal {
+  breed: string;
+
+  constructor(name: string, breed: string) {
+    super(name);
+    this.breed = breed;
   }
 
-  public accelearate(speedDelta: number): number {
-    if (this._speed + speedDelta <= this._maxSpeed) {
-      this._speed += speedDelta;
-      return this._speed;
-    }
-    this._speed = this._maxSpeed;
-    return this._speed;
-  }
-
-  public decelerate(speedDelta: number): number {
-    if (this._speed - speedDelta >= 0) {
-      this._speed -= speedDelta;
-      return this._speed;
-    }
-    this._speed = 0;
-    return this._speed;
-  }
-
-  public get speed(): number {
-    return this._speed;
-  }
-
-  public get manufacturer(): string {
-    return this._manufacturer;
-  }
-
-  public set manufacturer(value: string) {
-    this._manufacturer = value;
-  }
-
-  public get maxSpeed(): number {
-    return this._maxSpeed;
+  makeSound(): string {
+    return `${this.name} barks`;
   }
 }
 
-const  car = new Car('Lexus', 250)
-car.accelearate(150)
-car.decelerate(303)
-console.log(car.speed)
+const dog = new Dog("Pet", "Labrador");
 
+console.log(dog.makeSound());
